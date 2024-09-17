90673f51bd20854b6f2d132204adea373af681bf
qeqenn <168903182+qeqenn@users.noreply.github.com>
2024-09-17 12:57:36 +0800

    Update userandpassword.js

--- userandpassword.js
+++ userandpassword.js
@@ -1,6 +1,6 @@
 var username=new Array(256);
 var password=new Array(256);
 username[0]="Administrator"
-password[0]=2113
+password[0]=e807f1fcf82d132f9bb018ca6738a19f
 username[1]="local"
-password[1]=2281
+password[1]=e10adc3949ba59abbe56e057f20f883e
