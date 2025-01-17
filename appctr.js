0b6f80cfb8ae4bc6a1d1e7f782ed10b426b11be2
qeqenn <168903182+qeqenn@users.noreply.github.com>
2025-01-17 12:01:29 +0800

    Update appctr.js

--- appctr.js
+++ appctr.js
@@ -1,13 +1,24 @@
-var apps=0;
+var apps=1;
 var nowapp=0;
 var appname=new Array(256);
 var appverson=new Array(256);
 var appicon=new Array(256);
 var applocation=new Array(256);
+
+
+
 appname[0]="ShunBoiu Word";
 appverson[0]="V 1.0";
 appicon[0]="apps\\sbword\\sbword.bmp";
 applocation[0]="apps\\sbword\\sbword.html";
+
+appname[0]="Scenzid Maghned OS";
+appverson[0]="V 1.0";
+appicon[0]="https:\\\\qeqenn.github.io\\SM-OS\\logo.png";
+applocation[0]="https:\\\\qeqenn.github.io\\SM-OS\\";
+
+
+
 
 function getintoapp(){
 window.open(applocation[nowapp]);
